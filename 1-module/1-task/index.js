9df53d09652079c479d433de854b14deeba66861
Gogin <dmitry.gogin@t-systems.ru>
2021-09-10 16:58:50 +0300

    task 1 in module 1 solved

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,15 @@
 function factorial(n) {
-  // ваш код...
+  if ( n < 0 || n%1 != 0) {
+    return "Wrong argument! It must be a non-negative INTEGER like 0, 1, 2, 3 etc.";
+  }  
+  let result = undefined;
+  if (n == 0) {
+    result = 1;
+  } else {
+    result = n;
+  }
+  for (i = n; i > 1; i--) {
+    result = result * (i - 1);
+  }
+  return result;
 }
