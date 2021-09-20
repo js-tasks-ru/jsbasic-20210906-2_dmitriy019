9c574cc91c2ef48f99cdbbb9fc2675efa6315b60
Gogin <dmitry.gogin@t-systems.ru>
2021-09-20 22:58:37 +0300

    solutions for module 3 tasks 1, 2, 3

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,21 @@
+
+// let vasya = { name: 'Вася', age: 25 };
+// let petya = { name: 'Петя', age: 30 };
+// let masha = { name: 'Маша', age: 28 };
+
+// let users = [ vasya, petya, masha ];
+
 function namify(users) {
-  // ваш код...
+  let names = [];
+  for (let user of users) {
+    names.push(user.name);
+  }
+  return names;
 }
+
+
+// let names = namify(users);
+
+// console.log(names);
+
+
--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,10 @@
 function camelize(str) {
-  // ваш код...
+  let myArray = str.split("-");
+  for (let i = 1; i < myArray.length; i++) {
+    let item = myArray[i];
+    myArray[i] = item[0].toUpperCase() + item.substring(1, item.length);
+  }
+  return myArray.join("");
 }
+
+//console.log(camelize("-qwer-def"));
