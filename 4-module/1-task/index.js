51d1745a1171faf9492bf6bfffc45c644d4bc60f
Gogin <dmitry.gogin@t-systems.ru>
2021-09-26 23:41:09 +0300

    module 4: all 3 task solved

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  var ul = document.createElement('ul');
+
+  for (item of friends) {
+    var li = document.createElement('li');
+    var textnode = document.createTextNode(item.firstName + " " + item.lastName);
+    li.appendChild(textnode);
+    ul.appendChild(li);
+  }
+  return ul;
 }
